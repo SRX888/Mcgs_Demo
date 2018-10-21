d68b765347b72fe893bacde050a915eb04786d47
ruixiang.shen <shenruixiang10086@163.com>
2018-10-21 12:32:28 +0800

    modify adout devives for app

--- mcgs/src/main/java/com/mcgs/srx/McgsFragment/SetFragment.java
+++ mcgs/src/main/java/com/mcgs/srx/McgsFragment/SetFragment.java
@@ -1,8 +1,6 @@
 package com.mcgs.srx.McgsFragment;
 
-import android.content.ComponentName;
 import android.content.Context;
-import android.content.Intent;
 import android.os.Bundle;
 import android.os.Handler;
 import android.os.Message;
@@ -12,8 +10,7 @@ import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.Button;
-import android.widget.Toast;
+
 
 import com.mcgs.srx.R;
 import com.mcgs.srx.Util.JustifyTextView;
